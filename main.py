4e876146de4c54407af5f3d3e8b39b056a135b45
Lê Thanh Hùng <73764342+ThanhHung2112@users.noreply.github.com>
2020-12-05 18:00:36 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
-import string,random
-n = random.randrange(1,10)
-tuoi = random.randrange(1,100)
-def random_char(n):
-    return ''.join(random.choice(string.ascii_letters) for x in range(n))
-dict1 = {'Name':random_char(n),'Age':tuoi}
-print(dict1)
+import os
+name = input('nhập tên thư mục cần tạo :')
+os.mkdir(name)
+os.listdir()
+link=os.getcwd()
+name_file = input('nhập tên file :')
+m= open(name_file,'w')
+print(link)
+#n = input('nhập dữ liệu file')
+#m.write(n)
+#data = open(name_file).read()
+#print('dữ liệu trong tập tin là :', data)
+m.close()
+
+
